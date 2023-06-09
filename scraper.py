5f187d781437254d1fd2775f8fa7d22d30a571fd
Robson Glasscock <Robson@xbanalyticsllc.com>
2023-06-09 09:57:30 -0600

    third test commit after using git remote set-url origin to change from denver_assessor to zillow_scrape

--- scraper.py
+++ scraper.py
@@ -7,3 +7,5 @@ browser.get("https://zillow.com")
 # test commit after renaming the GitHub repo.
 
 # test commit 2, see if warning about repo moving cleared up on it's own
+
+# third test commit after using git remote set-url to update repo.
