7c9f3ca70d6759def438dd7f66a6d3269e67fe4d
Robson Glasscock <Robson@xbanalyticsllc.com>
2023-06-30 17:48:19 -0600

    Used pywebcopy to save down the website pages in a useful form. I found the last sold price for both 681 S. Gaylord and 695 S. Gaylord. I couldn't get trying to pull up the web page and feeding in CTRL + S to work. It seemed like doing this, the CTRL + S was ignored but subsequent ENTER or TAB's were accepted. These key inputs either toggeled the cursor through the site or pulled up the pictures of the houses. At this point, I think I have a useable template to pull all the housing data. I need to clean up or find a way to automate going through the directory structure from saving these to pull out the .html file which, for 681 S. Gaylord, is housed in: ~/wash_park_webscrape/pywebcopy/695 S Gaylord St Denver CO/www.redfin.com/CO/Denver/681-S-Gaylord-St-80209/home/34126393.html.

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+%reset -f
+
 from selenium import webdriver
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -5,15 +7,13 @@ import requests
 from time import sleep
 import random
 import os
+import pywebcopy
+from pywebcopy import save_webpage
 
-browser = webdriver.Chrome()
-
-# set path to download all files for Gaylord into a Gaylord folder.
-file_path = os.getcwd() + "/Gaylord"
-
-integer_hold = [681]
+integer_hold = [681, 695]
 # for i in range(600, 700, 1):
 for i in integer_hold:
+    browser = webdriver.Chrome()
     browser.get("https://redfin.com")
     # insert random sleep time for pull
     sleep(random.randint(2, 12))
@@ -26,149 +26,118 @@ for i in integer_hold:
     # feed in the text string with the appended enter key at the end into the box
     userElem.send_keys(text_str)
 
+    """ No longer needed since using pywebcopy: 
+
     # write out each file with the appropriate address.
     file_str = str(i) + " " + "S Gaylord St Denver CO.html"
     with open(file_path + "/" + file_str, "w") as f:
         f.write(browser.page_source)
     # last random sleep
     sleep(random.randint(10, 20))
+    """
 
-################
-%pwd
+    # set variable qual to current url include a sleep interval to let the page load before linking to the url. 
+    sleep(random.randint(10, 12))
 
-# see url
-browser.current_url
+    url_string = browser.current_url
+    print('the integer hold is:', integer_hold, '/n'
+          'the url string is:', url_string)
 
+    # kwargs= {'bypass_robots':True, 'project_name': 'recognisable-name'}
+    save_webpage(
+        url=url_string,
+        project_folder="/home/robson/wash_park_scrape/pywebcopy/",
+        project_name=text_str[:-1],
+        bypass_robots=True,
+        debug=True,
+        open_in_browser=False,
+        delay=None,
+        threaded=False,
+    )
 
-# set variable qual to current url. 
-url_string= browser.current_url
-browser.get(url_string)
-
-url_string
-
-import pywebcopy
-from pywebcopy import save_webpage
-
-
-url= url_string
-download_folder= %pwd
-
-url
-# kwargs= {'bypass_robots':True, 'project_name': 'recognisable-name'}
-save_webpage('https://www.redfin.com/CO/Denver/681-S-Gaylord-St-80209/home/34126392')
-
-https://www.redfin.com/CO/Denver/681-S-Gaylord-St-80209/home/34126392
-
-save_webpage(https://www.redfin.com/CO/Denver/681-S-Gaylord-St-80209/home/34126392)
-
-
-browser.current_url.page_source()
-browser.get(browser.current_url).page_source()
-
-browser.page_source
-print(browser.page_source)
-browser
-userElem
+""" Ended up not being able to use this. It seemed like the CTRL + S never hit the webpage 
+and only the ENTER and TAB calls were fed into the page. The plan below was to use CTRL + S to then save the page but this failed. 
 
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
+
+### Use below to click on webpage before saving? ###########
+clickable = browser.find_element_by_id("serach-box-input")
+ActionChains(browser).click(clickable).perform()
+
 # os.chdir("./tester")
 
 
-save_me = ActionChains(browser).key_down(Keys.CONTROL)\
-         .key_down('s').key_up(Keys.CONTROL).key_up('s')
+save_me = (
+    ActionChains(browser)
+    .key_down(Keys.CONTROL)
+    .key_down("s")
+    .key_up(Keys.CONTROL)
+    .key_up("s")
+    .key_down(Keys.F1)
+    .key_up(Keys.F1)
+    .key_down(Keys.F2)
+    .key_up(Keys.F2)
+    .key_down(Keys.TAB)
+    .key_up(Keys.TAB)
+    .key_down(Keys.TAB)
+    .key_up(Keys.TAB)
+    .key_down(Keys.ENTER)
+    .key_up(Keys.ENTER)
+)
+
+
+save_me = (
+    ActionChains(browser)
+    .key_down(Keys.CONTROL)
+    .key_down("s")
+    .key_up(Keys.CONTROL)
+    .key_up("s")
+    .key_down(Keys.F1)
+    .key_up(Keys.F1)
+    .key_down(Keys.F2)
+    .key_up(Keys.F2)
+    .key_down(Keys.TAB)
+    .key_up(Keys.TAB)
+    .key_down(Keys.TAB)
+    .key_up(Keys.TAB)
+    .key_down(Keys.ENTER)
+)
+
+
+save_me = (
+    ActionChains(browser)
+    .key_down(Keys.CONTROL)
+    .key_down("s")
+    .key_up(Keys.CONTROL)
+    .key_up("s")
+    .key_down(Keys.F1)
+    .key_up(Keys.F1)
+    .key_down(Keys.F2)
+    .key_up(Keys.F2)
+    .key_down(Keys.TAB)
+    .key_up(Keys.TAB)
+    .key_down(Keys.ENTER)
+)
+
+
+save_me = (
+    ActionChains(browser)
+    .key_down(Keys.ENTER)
+    .key_up(Keys.ENTER)
+    .key_down(Keys.CONTROL)
+    .key_down("s")
+    .key_up(Keys.CONTROL)
+    .key_up("s")
+)
+
+
+save_me = ActionChains(browser).key_down(Keys.CONTROL).key_down("s")
+
+
+# Above will show the popup to save the file into robson/Downloads but when the ENTER is hit it just brings the pictures up rather than saving into the Downloads folder...
+
 
 save_me.perform()
-
-browser.current_url
-
-################
-
-
-# find the url after entering the address.
-browser.current_url
-
-lister = []
-lister.append(browser.current_url)
-
-with open("page.html", "w") as f:
-    f.write(browser.page_source)
-
-lister
-
-
-browser.get("https://redfin.com")
-
-userElem = browser.find_element_by_id("search-box-input")
-
-userElem.send_keys("6829 E. Lowry Blvd. Denver CO" + "\n")
-browser.current_url
-
-lister.append(browser.current_url)
-
-
-lister
-
-
-#####################################
-from selenium import webdriver
-import pandas as pd
-from bs4 import BeautifulSoup
-import requests
-
-df = pd.DataFrame(data=None, columns=["url"])
-df
-
-browser = webdriver.Chrome()
-type(browser)
-browser.get("https://redfin.com")
-browser.implicitly_wait(1)
-
-userElem = browser.find_element_by_id("search-box-input")
-
-# input the address and then the enter key into the search box.
-userElem.send_keys("681 S Gaylord St Denver CO" + "\n")
-
-# find the url after entering the address.
-browser.current_url
-
-lister = []
-lister.append(browser.current_url)
-
-with open("page.html", "w") as f:
-    f.write(browser.page_source)
-
-lister
-
-
-browser.get("https://redfin.com")
-
-userElem = browser.find_element_by_id("search-box-input")
-
-userElem.send_keys("6829 E. Lowry Blvd. Denver CO" + "\n")
-browser.current_url
-
-lister.append(browser.current_url)
-
-
-lister
-
-
-##############
-
-with open("lawrence_complete.html") as fp:
-    soup = BeautifulSoup(fp, "html.parser")
-
-soup
-
-
-with open("lawrence_single.html") as fp:
-    soup = BeautifulSoup(fp, "html.parser")
-
-soup
-
-with open("lawrence_html.html") as fp:
-    soup = BeautifulSoup(fp, "html.parser")
-
-soup
+"""
