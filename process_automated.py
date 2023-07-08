fb0e97ab6115db117309159bcda64b21fde456d2
Robson Glasscock <Robson@xbanalyticsllc.com>
2023-07-08 13:48:02 -0600

    Examined the 487 addresses collected per Google Maps that were not on Redfin. I examined three of these by hand and they are cases where Redfin doesn't have any actual sales data for the house- only type of house, lot size, year built, and an estimate of FMV. Some of the addresses that were pulled down by pywebcopy have a second hyphen between the street name and S. A conditional logic statement was added to remove the duplicate hyphen, which was at the 10th from last position in the data when present. I don't know the reason for the second hyphen but I am thinking it has something to do with how the data is stored on Redfin. No exceptions noted at this point, and I believe the analysis is finished.

--- process_automated.py
+++ process_automated.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 import datetime
 
+pd.set_option('display.max_rows', None)
 #########################
 # All Firms
 #######################
@@ -73,6 +74,107 @@ for i in os.listdir():
         pass
 
 master_df
+
+master_df[master_df['address']=='942-S-Gaylord-St-80209']
+
+
+# Find missing addresses 
+os.chdir(pather)
+os.listdir()
+
+# Create a dataframe with addresses 
+hand_df= pd.DataFrame(data=os.listdir(), columns=['address_hand'])
+hand_df
+
+
+hand_df[hand_df['address_hand']=='942-S-Gaylord-St-Denver-CO-80209']
+hand_df[hand_df['address_hand']=='942-S-Gaylord--St-Denver-CO-80209']
+hand_df[hand_df['address_hand']=='1175-S-Williams-St-Denver-CO-80209']
+
+
+# Some houses were saved down by pywebcopy was a second hyphen between the street name and street. 
+
+# remove the "-Denver-CO" from the address string. 
+
+hand_df['address_hand']= hand_df['address_hand'].apply(lambda x: x.replace('-Denver-CO', ''))
+hand_df
+
+hand_df[hand_df['address_hand']=='942-S-Gaylord--St-80209']
+# two hypens persists. Remove this below. 10th spot back will be a hypen if there are two 
+
+hand_df['address_hand'][0]
+hand_df['address_hand'][0][-10]
+
+# Identify addresses that came in with double hyphens. 
+hand_df['double_hypen']=hand_df['address_hand'].apply(lambda x: x[-10]=='-')
+
+hand_df
+
+# if double_hypen =true, the 10th position should be replaced with ''
+
+# Scaffolding for index operations to remove the second hyphen if there are two hypens. 
+# index 1738 is for 558-S-Gaylord--St-80209
+hand_df['address_hand'].loc[1738]
+
+
+hand_df['address_hand'].loc[1738][:-10]
+hand_df['address_hand'].loc[1738][-10]
+hand_df['address_hand'].loc[1738][-10:]
+
+hand_df['address_hand'].loc[1738][:-9]
+hand_df['address_hand'].loc[1738][-9]
+hand_df['address_hand'].loc[1738][-9:]
+
+hand_df['address_hand'].loc[1738][-1]
+
+
+hand_df['address_hand'].loc[1738]
+
+hand_df['address_hand'].loc[1738][:-9] + hand_df['address_hand'].loc[1738][-8:]
+
+# Conditional logic to remove the duplicate hyphen. Note I ended up not using the double_hypen variable. 
+hand_df['address_hand']= hand_df['address_hand'].apply(lambda x: x[:-9] + x[-8:] if x[-10]=='-' else x)
+
+hand_df
+
+# merge dataframes 
+
+df_merged= pd.merge(hand_df, master_df, how='left', left_on='address_hand', right_on='address')
+df_merged
+
+# Any address without address, lastSoldPrice, and lastSoldDate was in the Google Maps address but not on RedFin. 
+df_discrep=df_merged[df_merged['address'].isna()]
+
+df_discrep
+
+hand_df.shape
+master_df.shape
+df_discrep.shape
+
+ # Logic check to see how many obs didn't merge and if this meets expections. p/f/r.  
+hand_df.shape[0] - master_df.shape[0] == df_discrep.shape[0]
+
+# 487 houses didn't merge with sales data. 
+
+df_discrep[df_discrep['address']== '1175-S-Williams-St-80210']
+master_df[master_df['address']== '1175-S-Williams-St-80210']
+hand_df[hand_df['address_hand']== '1175-S-Williams-St-80210']
+
+df_discrep[df_discrep['address']== '1175-S-Williams-St-80209']
+master_df[master_df['address']== '1175-S-Williams-St-80209']
+hand_df[hand_df['address_hand']== '1175-S-Williams-St-80209']
+
+
+# Examine a few houses below. Note that for the 1175-S-Williams the link has the zipcode as 80210 but in the automated search that pulled the house the zipcode was 80209: 
+
+# https://www.redfin.com/CO/Denver/1175-S-Williams-St-80210/home/34146228
+# https://www.redfin.com/CO/Denver/983-S-Gaylord-St-80209/home/34129178
+# https://www.redfin.com/CO/Denver/824-S-Franklin-St-80209/home/34127921
+
+# Each of the above has a different layout from RedFin. There is an estimate but no sales transactions in the dataset. It only has year built, property type, lot size, and Redfin Estimate. This appears to be the reason these 487 houses. p/f/r.  
+
+# Save the web scrape results as of around July 6th, 2023 to disk. 
+df_discrep.to_csv('/home/robson/wash_park_scrape/recon/discrep.csv', index=False)
 
 from datetime import datetime
 
